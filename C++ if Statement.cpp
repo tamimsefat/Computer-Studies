4e0715ea8b82dfb42b3963eafe870d88e1452e5c
tamimsefat <58241193+tamimsefat@users.noreply.github.com>
2019-12-04 08:40:25 +0600

    show positive number

---
+++ C++ if Statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    // checks if the number is positive
+    if ( number > 0)
+    {
+        cout << "You entered a positive integer: " << number << endl;
+    }
+    cout << "This statement is always executed.";
+    return 0;
+}
